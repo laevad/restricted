548c93154dc29c59e4487f1ff89611127b5190be
laevad <aldave.tabian@ustp.edu.ph>
2022-11-07 16:45:48 +0800

    init

---
+++ app/Http/Controllers/AssetsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
+
+class AssetsController extends Controller
+{
+
+    public function __invoke($file)
+    {
+        abort_if(auth()->guest(), Response::HTTP_FORBIDDEN);
+        $path = "secret/$file";
+
+        return response()->file(Storage::path($path));
+    }
+}
