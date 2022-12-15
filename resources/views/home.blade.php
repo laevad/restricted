4cf17fbefdb17b3a4ecb2d49c341f1d86158f982
laevad <aldave.tabian@ustp.edu.ph>
2022-12-15 11:40:40 +0800

    gotcha - secret in storage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-                <img src="{{ route('assets', 'cc.png') }}" alt="secret">
+                <img src="{{ route('assets', 'cc.jpg') }}" alt="secret">
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
